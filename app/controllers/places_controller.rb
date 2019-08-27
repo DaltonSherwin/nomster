13fd669e55b64aed9706d72f9ee8b33e1e386efe
DaltonSherwin <daltonsherwin@gmail.com>
2019-08-27 21:20:10 +0000

    Nomster Lesson 14

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,11 +1,25 @@
 class PlacesController < ApplicationController
-
   def index
     @places = Place.all
   end
 
-
   def new
     @place = Place.new
   end
+
+  def create
+  end
+
+
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
+
 end
