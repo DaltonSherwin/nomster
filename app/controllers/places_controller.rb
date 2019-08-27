f3028636ffd106e085d1fa035de59c3763704a87
DaltonSherwin <daltonsherwin@gmail.com>
2019-08-27 22:09:15 +0000

    Nomster Lesson 18

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,5 @@
 class PlacesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   def index
     @places = Place.all
   end
@@ -12,7 +13,7 @@ class PlacesController < ApplicationController
 
 
   def create
-    Place.create(place_params)
+    current_user.places.create(place_params)
     redirect_to root_path
   end
 
