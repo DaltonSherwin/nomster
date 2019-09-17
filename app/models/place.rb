97f7418c40f9c1595ea02ad558325d85f6aaf219
DaltonSherwin <daltonsherwin@gmail.com>
2019-09-17 00:37:34 +0000

    Nomster Lesson 31

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,9 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
+
   geocoded_by :address
   after_validation :geocode
-  validates :name, presence: true 
-  validates :address, presence: true
-  validates :description, presence: true
+
+  validates :name, presence: true
 end
