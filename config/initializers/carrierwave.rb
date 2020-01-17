fdbd6f1647011c6eef5158d40f08d2c2830a3b81
DaltonSherwin <daltonsherwin@gmail.com>
2020-01-17 02:41:16 +0000

    Nomster Done

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,11 +2,10 @@
 
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
-  config.fog_public = false
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AKIATY6KRIZ5V5USP6XL"],        # required
-    aws_secret_access_key: ENV["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"],        # required
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
   }
-  config.fog_directory  = ENV["daltons-nomster-images"]              # required
+  config.fog_directory  = ENV["AWS_BUCKET"]              # required
 end
