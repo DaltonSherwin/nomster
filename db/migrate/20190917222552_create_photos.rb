f81fe84441b8f6e7dbb9e16b9eecdd6f6504d53e
DaltonSherwin <daltonsherwin@gmail.com>
2020-01-25 20:59:51 +0000

    Nomster

--- db/migrate/20190917222552_create_photos.rb
+++ db/migrate/20190917222552_create_photos.rb
@@ -3,8 +3,9 @@ class CreatePhotos < ActiveRecord::Migration[5.2]
     create_table :photos do |t|
       t.text :caption
       t.integer :place_id
-
+      t.integer :user_id
       t.timestamps
     end
-  end
-end
+    add_index :photos, [:user_id, :place_id]
+    add_index :photos, :place_id
+  end 
